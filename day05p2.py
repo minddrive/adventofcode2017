8e45d0188758c284460f47b3655cc8e41e210505
Ken Lareau <elessar@numenor.org>
2017-12-04 21:08:48 -0800

    Day 5 both parts solved

---
+++ day05p2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+with open('input/day05.txt') as fh:
+    data = [int(x) for x in fh.read().strip().split('\n')]
+
+jumps = 0
+idx = 0
+
+while idx >= 0 and idx < len(data):
+    new_idx = idx + data[idx]
+    if data[idx] >= 3:
+       data[idx] -= 1
+    else:
+       data[idx] += 1
+    jumps += 1
+    idx = new_idx
+
+print(jumps)
