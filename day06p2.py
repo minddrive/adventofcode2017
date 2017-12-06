2f47a4a8497aa86f2c93e55694854f77099aa75f
Ken Lareau <elessar@numenor.org>
2017-12-05 21:26:01 -0800

    Day 6 both parts solved

---
+++ day06p2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+with open('input/day06.txt') as fh:
+    buckets = [int(x) for x in fh.read().strip().split()]
+
+cycles = 1
+configs = [list(buckets)]
+
+while True:
+    bucket = max(buckets)
+    b_idx = buckets.index(bucket)
+    buckets[b_idx] = 0
+
+    for cnt in range(b_idx + 1, b_idx + bucket + 1):
+        buckets[cnt % len(buckets)] += 1
+
+    if buckets in configs:
+        print(cycles - configs.index(buckets))
+        break
+
+    cycles += 1
+    configs.append(list(buckets))
